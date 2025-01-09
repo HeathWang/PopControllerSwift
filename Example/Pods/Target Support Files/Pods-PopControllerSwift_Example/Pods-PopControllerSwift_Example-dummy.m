b93ea39ea0c2f606b2855309476690e5672df4de
wangcongling <wangcongling06883@hellobike.com>
2025-01-09 15:11:37 +0800

    fixed untracked files

--- Example/Pods/Target Support Files/Pods-PopControllerSwift_Example/Pods-PopControllerSwift_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PopControllerSwift_Example : NSObject
-@end
-@implementation PodsDummy_Pods_PopControllerSwift_Example
-@end
--- Example/Pods/Target Support Files/PopControllerSwift/PopControllerSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PopControllerSwift : NSObject
-@end
-@implementation PodsDummy_PopControllerSwift
-@end
